100be8fcfdbb6373f99c64553fb2d2d0f4b4303a
bien.vu <tienbienit1992@gmail.com>
2015-08-22 08:56:20 +0700

    Add styleguilde file.

---
+++ js/script.js
@@ -0,0 +1,1 @@
+!function(o){o(document).ready(function(){var n=function(n){o("html, body").animate({scrollTop:0},1e3),n.preventDefault()};o(".go-to-top").on("click",n)})}(jQuery);
---
+++ readme.md
@@ -0,0 +1,10 @@
+# Build styleguide page
+
+1. cd styleguide
+2. npm install
+3. gulp
+
+# Infomation need know.
+- icoomoon: http://icomoon.io/
+- Susy
+- Gulp
