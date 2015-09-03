14395b4ef2b6ec95472f372fbe8d93a026428d23
tienbienit <tienbienit1992@gmail.com>
2015-09-03 11:32:13 +0700

    Update gulpfile.

--- js/script.js
+++ js/script.js
@@ -1,26 +1,5 @@
 // Script file
 $(document).ready(function () {
   "use strict";
-
-  // Menu responsive.
-  var processMenuMobile = function () {
-    $(this).toggleClass("active");
-    $('.region-header').toggleClass("active");
-    $('.sec-content').toggleClass("active");
-    return false;
-  };
-  
-  var processSubMenuMobile = function () {
-    $(this).parent("li").toggleClass("hover");
-    return false;
-  };
-
-  var processSubSubMenuMobile = function () {
-    $(this).toggleClass("active");
-    $(this).next('.sub-menu').toggleClass("active");
-  };
-
-  $('.toggleMenu').on('click', processMenuMobile);
-  $('.main-menu li a').on('click', processSubMenuMobile);
-  $('.navigation .main-menu li.sub-item > a').on('click', processSubSubMenuMobile);
+  // Code here.
 });
